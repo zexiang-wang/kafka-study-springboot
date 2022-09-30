328c9aa9c9bef9f2e69fe911ea859bba3782eb59
logan <17751758772@163.com>
2022-09-30 15:59:26 +0800

    second commit

---
+++ src/main/java/com/aze/kafkastudyspringboot/KafkaStudySpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.aze.kafkastudyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStudySpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStudySpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aze/kafkastudyspringboot/KafkaStudySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aze.kafkastudyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStudySpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
